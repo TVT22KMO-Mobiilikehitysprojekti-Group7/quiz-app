cdfae78df0c4c79d08d26dafe99c315d243ee5a8
Mika <mika.sarajarvi@gmail.com>
2023-11-16 10:24:23 +0200

    start,home,nickname alkuun

--- screens/Nickname.js
+++ screens/Nickname.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const Nickname = () => {
+  const [nickname, setNickname] = useState('');
+  const navigation = useNavigation();
+
   return (
     <View>
-      <Text>Nickname Screen</Text>
+      <TextInput
+        placeholder="Syötä pelaajan nimi"
+        value={nickname}
+        onChangeText={setNickname}
+      />
+      <Button
+        title="Valmis"
+        onPress={() => navigation.navigate('Home', { playerNickname: nickname })}
+      />
     </View>
   );
 };
