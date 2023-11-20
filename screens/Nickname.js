ae42fb8da9d5b1fed5672b3755faa671dcef880a
Sami Tapalinen <samitapal90@gmail.com>
2023-11-20 14:37:51 +0200

    Nickname settings and storage

--- screens/Nickname.js
+++ screens/Nickname.js
@@ -1,10 +1,75 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Button, Alert, ToastAndroid } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Nickname = () => {
-  const [nickname, setNickname] = useState('');
+  const route = useRoute();
   const navigation = useNavigation();
+  const [nickname, setNickname] = useState(route.params?.nickname);
+  const [storageNickname, setStorageNickname] = useState(null);
+
+  const getNicknameFromStorage = async () => {
+    try {
+      return await AsyncStorage.getItem('nickname');
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    getNicknameFromStorage().then((result) => {
+      if (result) {
+        setStorageNickname(result);
+      }
+    });
+  }, []);
+
+  const setNicknameToStorage = async (nickname) => {
+    try {
+      if (nickname) {
+        await AsyncStorage.setItem('nickname', nickname);
+        setNickname(nickname);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const changeNicknameAndResetPoints = () => {
+    setNicknameToStorage(nickname);
+    //TODO: Reset points
+    navigation.navigate('Home', { nickname: nickname })
+  }
+
+  const nicknameChangeAlert = () => 
+    Alert.alert('Huomio!', 'Nimimerkin vaihtaminen nollaa pisteesi. Haluatko varmasti vaihtaa nimimerkin?', [
+      {
+        text: 'Peruuta'
+      },
+      {
+        text: 'Kyllä',
+        onPress: () => changeNicknameAndResetPoints()
+      },
+    ],
+    {
+      cancelable: false,
+    }
+  );
+
+  const setNicknameAndNavigate = () => {
+    if (!nickname) {
+      ToastAndroid.show('Syötä nimimerkki!', ToastAndroid.SHORT);
+      return;
+    }
+    if (storageNickname && storageNickname != nickname) {
+      nicknameChangeAlert()
+    } else {
+      setNicknameToStorage(nickname)
+      navigation.navigate('Home', { nickname: nickname })
+    }
+  }  
 
   return (
     <View>
@@ -15,7 +80,7 @@ const Nickname = () => {
       />
       <Button
         title="Valmis"
-        onPress={() => navigation.navigate('Home', { playerNickname: nickname })}
+        onPress={() => setNicknameAndNavigate()}
       />
     </View>
   );
--- screens/Start.js
+++ screens/Start.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Start = () => {
   const navigation = useNavigation();
+  let nickname = null;
+
+  const getNicknameFromStorage = async () => {
+    try {
+      return await AsyncStorage.getItem('nickname');
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    getNicknameFromStorage().then((result) => {
+      if (result) {
+        nickname = result;
+      }
+    });
+  }, []);
 
   return (
     <Button
       title="Aloita"
-      onPress={() => navigation.navigate('Nickname')}
+      onPress={() => navigate()}
     />
   );
+
+  function navigate() {
+    if (nickname) {
+      navigation.navigate('Home', { nickname: nickname })
+    } else {
+      navigation.navigate('Nickname')
+    }
+  }
 };
 
 export default Start;
