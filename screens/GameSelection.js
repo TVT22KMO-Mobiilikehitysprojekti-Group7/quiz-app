2861ece7a8b88696512e00ebff996f3ac47c5043
Pasi Karpinmaa <t1kapa01@students.oamk.fi>
2023-11-25 17:07:37 +0200

    adding categories, created firebase for those categories

--- screens/GameSelection.js
+++ screens/GameSelection.js
@@ -12,10 +12,18 @@ const GameSelection = () => {
 
   return (
     <View>
-      <Button title="Historia" onPress={() => handleCategorySelect('Historia')} />
-      <Button title="Maantieto" onPress={() => handleCategorySelect('Maantieto')} />
-      {/* Lisää tarvittaessa muita kategorioita */}
-    </View>
+    <Button title="Historia" onPress={() => handleCategorySelect('Historia')} />
+    <Button title="Maantieto" onPress={() => handleCategorySelect('Maantieto')} />
+    <Button title="Tiede" onPress={() => handleCategorySelect('Tiede')} />
+    <Button title="Viihde" onPress={() => handleCategorySelect('Viihde')} />
+    <Button title="Elokuvat ja Sarjat" onPress={() => handleCategorySelect('ElokuvatJaSarjat')} />
+    <Button title="Urheilu" onPress={() => handleCategorySelect('Urheilu')} />
+    <Button title="Luonto" onPress={() => handleCategorySelect('Luonto')} />
+    <Button title="Teknologia" onPress={() => handleCategorySelect('Teknologia')} />
+    <Button title="Ruoka ja Juoma" onPress={() => handleCategorySelect('RuokaJaJuoma')} />
+    <Button title="Taide ja Kulttuuri" onPress={() => handleCategorySelect('TaideJaKulttuuri')} />
+    {/* Lisää tarvittaessa muita kategorioita */}
+  </View>
   );
 };
 
