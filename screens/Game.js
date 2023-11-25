a3a119760ebb14e9891949b6bc5c9016731d0df5
Pasi Karpinmaa <t1kapa01@students.oamk.fi>
2023-11-25 17:47:15 +0200

    Peli tarkistaa vastauksen, ja tekee asioita

--- screens/Game.js
+++ screens/Game.js
@@ -1,35 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { fetchQuestionsFromCategory } from '../data/dataService';
 
 const Game = ({ route }) => {
+  const navigation = useNavigation();
   const { category } = route.params;
+  const [fetchedQuestions, setFetchedQuestions] = useState([]);
   const [questions, setQuestions] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchQuestions = async () => {
       const fetchedQuestions = await fetchQuestionsFromCategory(category);
-      setQuestions(fetchedQuestions);
+
+      setFetchedQuestions(fetchedQuestions);
+
+      const questions = fetchedQuestions.map((question) => question.Kysymys);
+      const options = fetchedQuestions.map((question) => question.Vaihtoehdot);
+
+      setQuestions(questions);
+      setOptions(options);
       setLoading(false);
     };
 
     fetchQuestions();
   }, [category]);
 
+  const handleOptionSelect = (option) => {
+    const correctAnswer = fetchedQuestions[currentQuestionIndex].Vastaus;
+
+    if (option === correctAnswer) {
+      Alert.alert('Oikea vastaus', 'Hyvin tehty!', [{ text: 'OK' }]);
+      // Voit täällä päivittää pistemäärää tai tehdä muita toimia oikeasta vastauksesta
+      // Esimerkiksi: päivitäPistemäärää();
+    } else {
+      Alert.alert('Väärä vastaus', `Oikea vastaus oli: ${correctAnswer}`, [
+        {
+          text: 'OK',
+          onPress: () => navigation.goBack(), // Takaisin painikkeen logiikka väärässä vastauksessa
+        },
+      ]);
+    }
+  };
+
   if (loading) {
-    return <Text>Loading questions...</Text>;
+    return <Text>Ladataan kysymyksiä...</Text>;
   }
 
   return (
     <View>
-      {questions.map((question, index) => (
-        <View key={index}>
-          <Text>{question.Kysymys}</Text>
-          {question.Vaihtoehdot.map((option, idx) => (
-            <Button key={idx} title={option} onPress={() => {/* Vastauslogiikka */}} />
-          ))}
-        </View>
+      <Text>{questions[currentQuestionIndex]}</Text>
+      {options[currentQuestionIndex].map((option, idx) => (
+        <TouchableOpacity
+          key={idx}
+          onPress={() => handleOptionSelect(option)}
+          style={{ margin: 10, padding: 10, backgroundColor: 'lightgray', borderRadius: 5 }}
+        >
+          <Text>{option}</Text>
+        </TouchableOpacity>
       ))}
     </View>
   );
