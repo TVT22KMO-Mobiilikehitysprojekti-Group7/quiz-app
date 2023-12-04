2a24a0bf8fb7bbf00314845a6e7dd5f1ccf1d5c3
Pasi Karpinmaa <t1kapa01@students.oamk.fi>
2023-12-04 18:47:20 +0200

    Palaute pelistä

--- screens/Endgame.js
+++ screens/Endgame.js
@@ -3,25 +3,43 @@ import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { saveScore } from '../data/score';
 
-const Endgame = ({ route }) => {
-    const navigation = useNavigation();
-    const { score } = route.params;
+const getFeedback = (score) => {
+  if (score < 3000) {
+    return 'Kehityskelpoinen';
+  } else if (score >= 3000 && score < 6000) {
+    return 'Mukavasti';
+  } else if (score >= 6000 && score < 8000) {
+    return 'Mestari';
+  } else if (score >= 8000 && score < 12000) {
+    return 'Epic';
+  } else {
+    return 'Mastermind';
+  }
+};
 
-    useEffect(() => {
-      saveScore(score);
-    }, []);
-  
-    return (
-      <View>
-        <Text>Peli päättyi</Text>
-        <Text>Pisteesi: {score} </Text>
-        <Button
-          title="Palaa päävalikkoon"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
-    );
-   
+const Endgame = ({ route }) => {
+  const navigation = useNavigation();
+  const { score } = route.params;
+  const feedback = getFeedback(score);
+
+  useEffect(() => {
+    saveScore(score);
+  }, []);
+
+  return (
+    <View>
+      <Text>Peli päättyi</Text>
+      <Text>
+        Pisteesi: {score}
+        {"\n"}
+        Palautteesi: {feedback}
+      </Text>
+      <Button
+        title="Palaa päävalikkoon"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
 };
 
 export default Endgame;
