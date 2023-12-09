950bc6f72c7a1857c44146ebeacc0498edca490c
Pasi Karpinmaa <t1kapa01@students.oamk.fi>
2023-12-09 17:46:57 +0200

    Tilastot tallentuvat firebase ja top10 kaikkien pelaajien kesken palautetaan

--- data/score.js
+++ data/score.js
@@ -1,25 +1,61 @@
 // score.js
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit } from 'firebase/firestore';
 
+// Replace 'YourFirebaseConfig' with your actual Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-Nqj5ifCVjpZ7iNdlFTWufnL5hEvGX50",
+  authDomain: "tietoviisas-fe817.firebaseapp.com",
+  projectId: "tietoviisas-fe817",
+  storageBucket: "tietoviisas-fe817.appspot.com",
+  messagingSenderId: "772589460127",
+  appId: "1:772589460127:web:09a188af8a45088efe60aa"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Function to save local scores
 export const saveScore = async (score) => {
-  let localScores = await getLocalScores();
-  console.log("Existing scores:", localScores);
+  try {
+    // Save locally
+    let localScores = await getLocalScores();
+    console.log("Existing local scores:", localScores);
 
-  if (!localScores) {
-    localScores = [];
+    if (!localScores) {
+      localScores = [];
+    }
+
+    localScores.push(score);
+    if (localScores.length > 10) {
+      localScores.sort((a, b) => b.score - a.score);
+      localScores = localScores.slice(0, 10);
+    }
+
+    console.log("New local scores to save:", localScores);
+    await setLocalScores(localScores);
+
+    // Save to Firebase
+    const db = getFirestore(app);
+    const scoresCollection = collection(db, 'Scores');
+
+    // Retrieve the player nickname from AsyncStorage
+    const playerNickname = await AsyncStorage.getItem('playerNickname');
+
+    // Add the new score to Firestore along with the player nickname
+    await addDoc(scoresCollection, { score });
+
+    // Fetch the top 10 scores from Firestore
+    const firebaseScores = await fetchFirebaseScores();
+
+    console.log("Top 10 scores from Firebase:", firebaseScores);
+  } catch (error) {
+    console.error("Error saving score to Firebase:", error);
   }
-
-  localScores.push(score);
-  if (localScores.length > 10) {
-    localScores.sort((a, b) => b.score - a.score);
-    localScores = localScores.slice(0, 10);
-  }
-
-  console.log("New scores to save:", localScores);
-  await setLocalScores(localScores);
 }
 
+// Function to retrieve local scores
 export const getLocalScores = async () => {
   try {
     const jsonValue = await AsyncStorage.getItem(`local_scores`);
@@ -30,6 +66,7 @@ export const getLocalScores = async () => {
   }
 };
 
+// Function to set local scores
 const setLocalScores = async (scores) => {
   try {
     const jsonValue = JSON.stringify(scores);
@@ -39,10 +76,31 @@ const setLocalScores = async (scores) => {
   }
 };
 
+// Function to reset local scores
 export const resetLocalScores = async () => {
   try {
     await AsyncStorage.setItem('local_scores', JSON.stringify([]));
   } catch (error) {
     console.error("Error resetting local scores: ", error);
+  }
+};
+
+// Function to fetch top scores from Firebase
+export const fetchFirebaseScores = async () => {
+  try {
+    const db = getFirestore(app);
+    const scoresCollection = collection(db, 'Scores');
+
+    // Fetch the top 10 scores from Firestore
+    const querySnapshot = await getDocs(query(scoresCollection, orderBy('score', 'desc'), limit(10)));
+    
+    // Extract and log the scores
+    const newFirebaseScores = querySnapshot.docs.map(doc => doc.data().score);
+    console.log("Top 10 scores from Firebase:", newFirebaseScores);
+
+    return newFirebaseScores;
+  } catch (error) {
+    console.error('Error fetching scores from Firebase:', error);
+    return []; // Handle the error by returning an empty array or a default value
   }
 };
--- screens/Scoreboard.js
+++ screens/Scoreboard.js
@@ -1,79 +1,135 @@
 // Scoreboard.js
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, Button, FlatList, TouchableOpacity, ImageBackground, Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { getLocalScores } from '../data/score';
+import { fetchFirebaseScores, getLocalScores } from '../data/score'; // Import the new functions
 
 const Scoreboard = () => {
   const navigation = useNavigation();
   const [localScores, setLocalScores] = useState([]);
+  const [firebaseScores, setFirebaseScores] = useState([]);
   const [showAllScores, setShowAllScores] = useState(true);
 
   useEffect(() => {
     const loadScores = async () => {
-      const scores = await getLocalScores();
-      console.log(scores);
-      setLocalScores(scores);
+      const localScoresData = await getLocalScores();
+      setLocalScores(localScoresData);
+
+      try {
+        const firebaseScoresData = await fetchFirebaseScores();
+        setFirebaseScores(firebaseScoresData);
+      } catch (error) {
+        console.error('Error fetching scores from Firebase:', error);
+        setFirebaseScores([]); // Handle the error by setting an empty array or a default value
+      }
     };
+
     loadScores();
   }, []);
 
-  // Järjestä tulokset ennen FlatListiin syöttämistä
-  const sortedScores = localScores && localScores.length > 0
-    ? localScores.sort((a, b) => b.score - a.score)
-    : [];
+  const handleTop10Press = async () => {
+    try {
+      const newFirebaseScores = await fetchFirebaseScores();
+      setFirebaseScores(newFirebaseScores);
+      setShowAllScores(false); // Show Top10
+    } catch (error) {
+      console.error('Error fetching scores from Firebase:', error);
+      setFirebaseScores([]); // Handle the error by setting an empty array or a default value
+    }
+  };
 
-  // Filteröi tulokset näytettäväksi
+  const sortedScores = (source) =>
+    source && source.length > 0 ? source.sort((a, b) => b.score - a.score) : [];
+
   const scoresToDisplay = showAllScores
-    ? sortedScores
-    : sortedScores.filter(item => {
-      const today = new Date().setHours(0, 0, 0, 0);
-      const scoreDate = new Date(item.date).setHours(0, 0, 0, 0);
-      return scoreDate === today;
-    });
+    ? (localScores?.length > 0 ? sortedScores(localScores) : [])
+    : sortedScores(firebaseScores);
 
   return (
-    <View>
-      <Text> Scoreboard </Text>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 10 }}>
-        <TouchableOpacity
-          style={{
-            padding: 10,
-            backgroundColor: showAllScores ? '#ccc' : '#fff',
-            borderRadius: 5,
-          }}
-          onPress={() => setShowAllScores(false)}
-        >
-          <Text>Kaikki</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            padding: 10,
-            backgroundColor: showAllScores ? '#fff' : '#ccc',
-            borderRadius: 5,
-          }}
-          onPress={() => setShowAllScores(true)}
-        >
-          <Text>Tänään</Text>
-        </TouchableOpacity>
+    <ImageBackground
+      source={require('../assets/tietoviisas-on-screen.png')}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Scoreboard</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[
+              styles.button,
+              { backgroundColor: showAllScores ? '#ccc' : '#fff' },
+            ]}
+            onPress={() => setShowAllScores(false)}
+          >
+            <Text>Kaikki</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.button,
+              { backgroundColor: showAllScores ? '#fff' : '#ccc' },
+            ]}
+            onPress={() => setShowAllScores(true)}
+          >
+            <Text>Tänään</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.button,
+              { backgroundColor: showAllScores ? '#fff' : '#ccc' },
+            ]}
+            onPress={handleTop10Press}
+          >
+            <Text>Top10</Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={scoresToDisplay}
+          renderItem={({ item }) => (
+            <View style={styles.scoreItem}>
+              <Text>Player: {item.playerNickname}</Text>
+              <Text>Score: {item.score}</Text>
+            </View>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+        <Button title="Takaisin" onPress={() => navigation.goBack()} />
       </View>
-      <FlatList
-        data={scoresToDisplay}
-        renderItem={({ item }) => (
-          <View>
-            <Text>Score: {item.score}</Text>
-            <Text>Date: {new Date(item.date).toLocaleDateString()}</Text>
-          </View>
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-      <Button
-        title="Takaisin"
-        onPress={() => navigation.goBack()}
-      />
-    </View>
+    </ImageBackground>
   );
+};
+
+const styles = {
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'white',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  scoreItem: {
+    marginBottom: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: 10,
+    borderRadius: 5,
+  },
 };
 
 export default Scoreboard;
